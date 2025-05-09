8ebeb702c4a68da98822eca89e44bc36ec90fc1e
foreverallama <klockatoo@gmail.com>
2025-05-09 16:10:44 +0530

    MAINT: Functional changes to matio project to allow for reading different MAT-file versions 1. Created new file `matio_base.py` which contains the base methods for reading mat-files 2. Renamed `readmat.py` to `matio5.py` as it reads mat v7 files

---
+++ matio/matio5.py
@@ -0,0 +1,151 @@
+"""Reads MAT-files v7 to v7.2 (MAT-file 5) and extracts variables including MATLAB objects"""
+
+from io import BytesIO
+
+import numpy as np
+from scipy.io import loadmat
+from scipy.io.matlab._mio5 import MatFile5Reader
+from scipy.io.matlab._mio5_params import OPAQUE_DTYPE
+
+from matio.subsystem import SubsystemReader
+
+
+class MatRead5:
+    """Reads MAT-files and extracts variables including MATLAB objects"""
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_subsystem(
+        self,
+        ssdata,
+        byte_order,
+        mat_dtype,
+        verify_compressed_data_integrity,
+    ):
+        """Reads subsystem data as a MAT-file stream
+        Inputs
+            1. ssdata (numpy.ndarray): Subsystem data from "__function_workspace__"
+            2. kwargs: Additional arguments for scipy.io.loadmat
+        Returns:
+            subsystem data (numpy.ndarray): Parsed subsystem data
+        """
+        ss_stream = BytesIO(ssdata)
+
+        ss_stream.seek(8)  # Skip subsystem header
+        subsystem_reader = MatFile5Reader(
+            ss_stream,
+            byte_order=byte_order,
+            mat_dtype=mat_dtype,
+            verify_compressed_data_integrity=verify_compressed_data_integrity,
+        )
+        subsystem_reader.initialize_read()
+        hdr, _ = subsystem_reader.read_var_header()
+        try:
+            res = subsystem_reader.read_var_array(hdr, process=False)
+        except Exception as err:
+            raise ValueError(f"Error reading subsystem data: {err}") from err
+
+        return res
+
+    def find_opaque_dtype(self, arr, subsystem, path=()):
+        """Recursively finds and replaces mxOPAQUE_CLASS objects in a numpy array
+        with the corresponding MCOS object.
+
+        This is a hacky solution to find mxOPAQUE_CLASS arrays inside struct arrays or cell arrays.
+        """
+
+        if not isinstance(arr, np.ndarray):
+            return arr
+
+        if arr.dtype == OPAQUE_DTYPE:
+            type_system = arr[0]["_TypeSystem"]
+            metadata = arr[0]["_Metadata"]
+            return subsystem.read_mcos_object(metadata, type_system)
+
+        if arr.dtype == object:
+            # Iterate through cell arrays
+            for idx in np.ndindex(arr.shape):
+                cell_item = arr[idx]
+                if cell_item.dtype == OPAQUE_DTYPE:
+                    type_system = cell_item[0]["_TypeSystem"]
+                    metadata = cell_item[0]["_Metadata"]
+                    arr[idx] = subsystem.read_mcos_object(metadata, type_system)
+                else:
+                    self.find_opaque_dtype(cell_item, subsystem, path + (idx,))
+
+        elif arr.dtype.names:
+            # Iterate though struct array
+            for idx in np.ndindex(arr.shape):
+                for name in arr.dtype.names:
+                    field_val = arr[idx][name]
+                    if field_val.dtype == OPAQUE_DTYPE:
+                        type_system = field_val[0]["_TypeSystem"]
+                        metadata = field_val[0]["_Metadata"]
+                        arr[idx][name] = subsystem.read_mcos_object(
+                            metadata, type_system
+                        )
+                    else:
+                        self.find_opaque_dtype(field_val, subsystem, path + (idx, name))
+
+        return arr
+
+    def read_matfile(
+        self,
+        raw_data=False,
+        add_table_attrs=False,
+        spmatrix=True,
+        byte_order=None,
+        mat_dtype=False,
+        chars_as_strings=True,
+        verify_compressed_data_integrity=True,
+        variable_names=None,
+    ):
+        """Loads variables from MAT-file < v7.3
+        Calls scipy.io.loadmat to read the MAT-file and then processes the
+        "__function_workspace__" variable to extract subsystem data.
+        Inputs
+            1. raw_data (bool): Whether to return raw data for objects
+            2. add_table_attrs (bool): Add attributes to pandas DataFrame
+            3. spmatrix (bool): Additional arguments for scipy.io.loadmat
+            4. byte_order (str): Endianness
+            5. mat_dtype (bool): Whether to load MATLAB data types
+            6. chars_as_strings (bool): Whether to load character arrays as strings
+            7. matlab_compatible (bool): Whether to load MATLAB compatible data types
+            8. verify_compressed_data_integrity (bool): Whether to verify compressed data integrity
+            9. variable_names (list): List of variable names to load
+        Returns:
+            1. matfile_dict (dict): Dictionary of loaded variables
+        """
+
+        matfile_dict = loadmat(
+            self.file_path,
+            spmatrix=spmatrix,
+            byte_order=byte_order,
+            mat_dtype=mat_dtype,
+            chars_as_strings=chars_as_strings,
+            verify_compressed_data_integrity=verify_compressed_data_integrity,
+            variable_names=variable_names,
+        )
+        ssdata = matfile_dict.pop("__function_workspace__", None)
+        if ssdata is None:
+            # No subsystem data in file
+            return matfile_dict
+
+        if byte_order is None:
+            byte_order = "<" if ssdata[0, 2] == b"I"[0] else ">"
+
+        ss_array = self.read_subsystem(
+            ssdata,
+            byte_order,
+            mat_dtype,
+            verify_compressed_data_integrity,
+        )
+        subsystem = SubsystemReader(ss_array, byte_order, raw_data, add_table_attrs)
+
+        for var, data in matfile_dict.items():
+            if not isinstance(data, np.ndarray):
+                continue
+            matfile_dict[var] = self.find_opaque_dtype(data, subsystem)
+
+        return matfile_dict
